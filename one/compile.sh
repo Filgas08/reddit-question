5756644bb3831e1105f48a74bcac635ca30a89ad
Filgas08 <filgas08@gmail.com>
2022-05-13 18:56:26 +0200

    first commit

---
+++ one/compile.sh
@@ -0,0 +1,2 @@
+/home/filgas08/vulkan/1.3.204.1/x86_64/bin/glslc shaders/simple_shader.vert -o shaders/simple_shader.vert.spv
+/home/filgas08/vulkan/1.3.204.1/x86_64/bin/glslc shaders/simple_shader.frag -o shaders/simple_shader.frag.spv
